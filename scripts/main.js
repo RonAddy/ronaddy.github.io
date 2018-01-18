2ab8a4b8b903f4e575f0675f99de0ddc6de0a07a
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-01-18 16:41:27 -0500

    Added loading bar and logo to page

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+console.log("jQuery loaded !")
+
+function logoAnimate() {
+	
+	$(window).on('load' , function() {
+    	$("#white_outline").delay(2000).slideUp(3000)
+	});
+	
+		function move() {
+			var elem = document.getElementById('loading_progress')
+		    var width = 10;
+		    var id = setInterval(frame, 45);
+		    function frame() {
+		        if (width >= 100) {
+		            clearInterval(id);
+		        } else {
+		            width++; 
+		            elem.style.width = width + '%'; 
+		            elem.innerHTML = width * 1 + '%';
+		        }
+		    }
+}
+	
+	move()
+}
+
+logoAnimate()
